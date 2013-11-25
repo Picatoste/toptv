3a9166d5934af9c8017ed9f27c9189353d89cf83
dabenito@gmail.com <Gorgorito85>
2013-11-25 17:38:10 +0100

    like button fb

--- src/views/entrada_view.js
+++ src/views/entrada_view.js
@@ -94,7 +94,7 @@ SocialSharingView = Backbone.View.extend({
   },
  
   template:
-	'<div class="left margin_left_10"><fb:like href="<%= readUrl %>" send="false" layout="button_count" show_faces="false"></fb:like></div>' +
+	'<div class="fb-like" data-href="<%= encodeURIComponent(readUrl) %>" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>' +
     '<div class="left margin_left_10"><a href="https://www.facebook.com/dialog/send?app_id=595636333827563&name=SolucionesDBR - <%= encodeURIComponent(name) %>&description=<%= encodeURIComponent(message) %>&link=<%= encodeURIComponent(readUrl) %>&redirect_uri=https://apps.facebook.com/solucionesDBR/" target="_blank"><img class="right selectable" src="img/facebook_orange.png" title="Compartir facebook" alt="Compartir FB"></img></a></div>' +
     '<div class="left margin_left_10"><a href="https://twitter.com/share?url=<%= encodeURIComponent(readUrl) %>&text=<%= encodeURIComponent(name) %>" target="_blank"><img class="right selectable" src="img/twitter_orange.png" title="Compartir twitter" alt="Compartir TW"></img></a></div>'+
     '<div class="left margin_left_10"><a href="mailto:friend@somewhere.com?subject=SolucionesDBR - <%= name %>&body=by <%= author %>, <%= message %> <%= readUrl %>" target="_blank"><img class="right selectable" src="img/email_orange.png" title="Compartir email" alt="Compartir Email"></img></a></div>' +
