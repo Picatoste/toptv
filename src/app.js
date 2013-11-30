baaf0fcbbd989e5949a2c37c1f60b98dbe654ef8
dabenito@gmail.com <Gorgorito85>
2013-11-30 18:19:57 +0100

    -

--- src/app.js
+++ src/app.js
@@ -88,8 +88,8 @@ var App = new (Backbone.Router.extend({
     $('#app-side').html(entradasView.el);
 
     //pedimos datos al server
-    this.activeList.fetch(null, n);
-	this.activeList = this.activeList.first(nelements);
+    this.activeList.fetch(null, nelements);
+	
     //render
     //no haria falta si cargamos del server ya que reaccionamos a los eventos :)
     entradasView.render();
